48226c46216cb68d5b97805c75c4b992de453633
Gapragapr <gapragapr3@mail.ru>
2022-02-25 02:22:46 +0600

    add 1,2 quests

--- src/js/__tests__/main-tests.js
+++ src/js/__tests__/main-tests.js
@@ -1,4 +1,5 @@
 import MathCharacter from '../MathChar';
+import ArrayBufferConverter from '../ArrayBuf';
 
 test('return stoned decrease attack', () => {
   const merlin = new MathCharacter('Merlin', 100, true, 2);
@@ -8,4 +9,12 @@ test('return stoned decrease attack', () => {
 test('return usual decrease attack', () => {
   const baltazar = new MathCharacter('Baltazar', 100, false, 3);
   expect(baltazar.attack).toBe(80);
+});
+
+test('return object', () => {
+  const buffer = new ArrayBufferConverter();
+  const result = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+  buffer.load(result);
+
+  expect(buffer.toStr()).toEqual(result);
 });
